949b969323039295a1120a8c94011fb026049a40
Yafira <yafira.martinez@lc.cuny.edu>
2021-08-11 17:10:09 -0400

    implemented a 'more about me' button

--- script.js
+++ script.js
@@ -15,4 +15,19 @@ window.onclick = function(event) {
     if (event.target == modal) {
     modal.style.display = "none";
     }
-}
+};
+
+// about-more, about-less buttons
+const aboutMore = document.querySelector('.about-more')
+const aboutLess = document.querySelector('.about-less')
+const about = document.querySelector('.about')
+
+aboutMore.addEventListener('click', () => {
+    about.classList.remove('hide')
+    aboutMore.classList.add('hide')
+})
+
+aboutLess.addEventListener('click', () => {
+    about.classList.add('hide')
+    aboutLess.classList.remove('hide')
+})
