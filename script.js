66a3dd02b5ca129877d2c918f530fbe6c4ce341f
Yafira <yafira.martinez@lc.cuny.edu>
2021-09-26 23:58:11 -0400

    removing previous code

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// dark mode
-
-// wait for the page to load
-document.addEventListener('DOMContentLoaded', () => {
-    const themeStylesheet = document.getElementById('theme')
-    const themeToggle = document.getElementById('theme-toggle')
-    themeToggle.addEventListener('click', () => {
-      // if it's light -> go dark
-    if (themeStylesheet.href.includes('light')) {
-        themeStylesheet.href = 'dark-theme.css'
-        themeToggle.innerText = 'Switch to light mode'
-    } else {
-        // if it's dark -> go light
-        themeStylesheet.href = 'light-theme.css'
-        themeToggle.innerText = 'Switch to dark mode'
-        }
-        // save the preference to localStorage
-        localStorage.setItem('theme',themeStylesheet.href)
-    })
-})
-
 // modal window
 const modal = document.getElementById("myModal");
 const btn = document.getElementById("myBtn");
