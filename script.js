4107a0f6dff509eef3c2328191089c382ac899ea
Yafira <yafira.martinez@lc.cuny.edu>
2021-09-26 23:45:51 -0400

    toggle dark mode with js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// dark mode
+
+// wait for the page to load
+document.addEventListener('DOMContentLoaded', () => {
+    const themeStylesheet = document.getElementById('theme')
+    const themeToggle = document.getElementById('theme-toggle')
+    themeToggle.addEventListener('click', () => {
+      // if it's light -> go dark
+    if (themeStylesheet.href.includes('light')) {
+        themeStylesheet.href = 'dark-theme.css'
+        themeToggle.innerText = 'Switch to light mode'
+    } else {
+        // if it's dark -> go light
+        themeStylesheet.href = 'light-theme.css'
+        themeToggle.innerText = 'Switch to dark mode'
+        }
+    })
+})
+
 // modal window
 const modal = document.getElementById("myModal");
 const btn = document.getElementById("myBtn");
